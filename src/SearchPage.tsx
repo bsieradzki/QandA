d66c50dcf92962905f1035829f4724e62f407a8b
bsierad <bsierad@hotmail.com>
2021-02-27 06:34:09 -0500

    react router; router parameters; useSearchParams() hook;

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,6 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useSearchParams } from 'react-router-dom';
+import { QuestionList } from './QuestionList';
+import { searchQuestions, QuestionData } from './QuestionsData';
+
 import React from 'react';
 import { Page } from './Page';
 
+
 export const SearchPage = () => {
-    return <Page title="Search Results">{null}</Page>
+    //destructure the query parameter(s) via the useSearchParams() react hook
+    //this hook returns two elements, the first contains the search params
+    //the second (not using here) is a function to update the query params
+    const [searchParams] = useSearchParams();
+    const [questions, setQuestions] = React.useState<QuestionData[]>([]);
+    const search = searchParams.get("criteria") || "";
+
+    React.useEffect(
+        () => {
+            const doSearch = async (criteria: string ) => {
+                const foundResults = await searchQuestions(criteria);
+                setQuestions(foundResults);
+            };
+            doSearch(search);            
+        },
+        [search]   
+    )
+
+    return <Page title="Search Results">
+        {search && (
+            <p 
+                css={css`
+                    font-size: 16px;
+                    font-style: italic;
+                    margin-top: 0px;
+            `}
+            >
+                for "{search}"
+            </p>
+        )}
+        <QuestionList data={questions} />
+        </Page>
 };
