53cb73790d8a3dc2ff8b836a3bac407464afe169
bsieradzki <bsierad@hotmail.com>
2021-03-14 07:29:38 -0400

    initial setup of backend with Dapper

---
+++ Backend/Data/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using QandA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(String search);
+        IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+        QuestionGetSingleResponse GetQuestion(int questionId);
+        bool QuestionExists(int questionId);
+        AnswerGetResponse GetAnswer(int answerId);
+    }
+}
